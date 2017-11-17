7f5cdb2b047caa0588561e9e9b9e4021c329e18f
qing.hua <qing.hua@hongshenol.com>
2017-11-17 11:22:43 +0800

    add file

---
+++ src/main/java/com/hongshen/sran_service/service/impl/AuthorityService_Unicom_Lte.java
@@ -0,0 +1,23 @@
+package com.hongshen.sran_service.service.impl;
+
+import com.hongshen.sran_service.dao.UnicomAuthorityWcdmaMapper;
+import com.hongshen.sran_service.service.AuthorityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * Created by poplar on 11/13/17.
+ */
+@Service
+public class AuthorityService_Unicom_Lte implements AuthorityService {
+
+//    @Autowired
+//    private UnicomAuthorityLteMapper authorityMapper;
+
+//    @Override
+//    public Map<String, Object> getUserAuthorty(String name) {
+//        return null;
+//    }
+}
