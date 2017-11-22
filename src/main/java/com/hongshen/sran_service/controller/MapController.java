deaf971f00e9d3ec79cd5d16f2f0724f1e9eccbb
xuwei.qiao <xuwei.qiao@hongshenol.com>
2017-11-22 13:40:52 +0800

    MapCotroller

--- src/main/java/com/hongshen/sran_service/controller/MapController.java
+++ src/main/java/com/hongshen/sran_service/controller/MapController.java
@@ -27,13 +27,13 @@ public class MapController extends BaseController{
         JSONObject result = new JSONObject();
         /*if (check(url, method, authToken)) {*/
             NetObjBase obj = objFactory.getNetObj(supplier, generation);
-            List<JSONObject> quo = obj.getElementInfoService().getGroupList();
+            List<JSONObject> grupList = obj.getElementInfoService().getGroupList();
             List<JSONObject> List =new ArrayList<>();
-            for (int i = 0;i<quo.size();i++){
+            for (int i = 0;i<grupList.size();i++){
                 JSONObject result1 = new JSONObject();
                 List<Double[]> list = new ArrayList<>();
-                if (quo.get(i).getString("group_name") != null) {
-                    String groupName= quo.get(i).getString("group_name");
+                if (grupList.get(i).getString("group_name") != null) {
+                    String groupName= grupList.get(i).getString("group_name");
                     List<JSONObject> nodeList =  obj.getElementInfoService().getNodeList(groupName);
                     JSONObject group = obj.getElementInfoService().getGroupByName(groupName);//TODO
                     Double latitude = 0.0;
@@ -42,10 +42,12 @@ public class MapController extends BaseController{
                     if (nodeList.size() != 0) {
                         for (int j = 0; j < nodeList.size(); j++) {
                             num++;
-                            Double[] doubles ={nodeList.get(j).getDouble("latitude"),nodeList.get(j).getDouble("longitude")};
-                            list.add(doubles);
-                            latitude = latitude  + nodeList.get(j).getDouble("latitude");
-                            longitude = longitude + nodeList.get(j).getDouble("longitude");
+                            if(nodeList.get(j).getDouble("latitude")!= 0.0 && nodeList.get(j).getDouble("longitude")!= 0.0){
+                                Double[] doubles ={nodeList.get(j).getDouble("latitude"),nodeList.get(j).getDouble("longitude")};
+                                list.add(doubles);
+                                latitude = latitude  + nodeList.get(j).getDouble("latitude");
+                                longitude = longitude + nodeList.get(j).getDouble("longitude");
+                            }
                         }
                     }
                     JSONObject json = Scopes(latitude,longitude,num);
@@ -62,8 +64,6 @@ public class MapController extends BaseController{
             }
             result.put("data", List);
             result.put("result", Constants.SUCCESS);
-            System.out.println(result);
-
             return result;
        /* }else{
                 result.put("result", Constants.FAIL);
@@ -86,18 +86,46 @@ public class MapController extends BaseController{
         }
         return j;
     }
-//    @GET
-//    @Path("/suppliers/{supplier}/generations/{generation}/nets/groups/{groupName}/nodes/mapinfos")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public JSONObject  getNodeList(@PathParam("supplier")String supplier, @PathParam("generation")String generation,
-//                                    @HeaderParam("Auth-Token")String authToken,@PathParam("groupName")String groupName) {
-//        System.out.println(groupName);
-//        JSONObject result = new JSONObject();
-//        NetObjBase obj = objFactory.getNetObj(supplier, generation);
-//        ElementInfoServiceQuotaService elementInfoServiceQuotaService = obj.getNodeList(groupName);
-//        List<JSONObject> nodeList = elementInfoServiceQuotaService.getNodeList(groupName);
-//        System.out.println(nodeList);
-//        result.put("ss",nodeList);
-//        return result;
-//    }
+    @GET
+    @Path("/suppliers/{supplier}/generations/{generation}/nets/groups/{groupName}/nodes/mapinfos")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JSONObject  getNodeList(@PathParam("supplier")String supplier, @PathParam("generation")String generation,
+                                    @HeaderParam("Auth-Token")String authToken,@PathParam("groupName")String groupName) {
+
+        JSONObject result = new JSONObject();
+        NetObjBase obj = objFactory.getNetObj(supplier, generation);
+        List<JSONObject> nodeList = obj.getElementInfoService().getNodeList(groupName);
+        JSONObject json = new JSONObject();
+        List list = new ArrayList();
+        if(!nodeList.isEmpty()){
+            for(int i=0;i<nodeList.size();i++){
+                JSONObject nodeLevel = obj.getQuotaService().getNodeLevel(nodeList.get(i).getString("name"));
+                if(!nodeLevel.isEmpty()){
+                    json.put("level",nodeLevel.getString("level"));
+                    json.putAll(nodeList.get(i));
+                    list.add(json);
+                }else{
+
+                }
+            }
+            result.put("data",list);
+            result.put("result", Constants.SUCCESS);
+        }else{
+            result.put("result",Constants.FAIL);
+        }
+        return result;
+    }
+
+    @GET
+    @Path("/suppliers/{supplier}/generations/{generation}/nets/groups/{groupName}/nodes/{nodeName}/cells/mapinfos")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JSONObject  getCellList(@PathParam("supplier")String supplier, @PathParam("generation")String generation,
+                                   @HeaderParam("Auth-Token")String authToken,@PathParam("groupName")String groupName,
+                                   @PathParam("nodeName")String nodeName) {
+        JSONObject result = new JSONObject();
+        NetObjBase obj = objFactory.getNetObj(supplier, generation);
+        List<JSONObject> cellList = obj.getElementInfoService().getCellList(nodeName);
+
+        return  null;
+    }
 }
