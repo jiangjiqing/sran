d531d58e82bc3d396e7a3d770f86a0be6e0da0d6
hs-server3 <git>
2017-12-01 17:00:19 +0800

    map

--- src/main/java/com/hongshen/sran_service/service/impl/ScannerService_Unicom_Lte.java
+++ src/main/java/com/hongshen/sran_service/service/impl/ScannerService_Unicom_Lte.java
@@ -507,6 +507,171 @@ public class ScannerService_Unicom_Lte implements ScannerService{
         return result;
     }
 
+    public String groupCalculationOld(JSONObject params, String time){
+
+        String result = null;
+
+        if (params == null || time == null) {
+
+            return null;
+        }
+
+        List<String> paramValues = new ArrayList<>();
+
+        List<String> paramcloumns = new ArrayList<>();
+
+        paramcloumns.add("name");
+        paramcloumns.add("time");
+        paramcloumns.add("level");
+
+        Map<String, List<String>> expressionSetMap = null;//getVariableListWcdma();
+
+        Map<String, List<String>> nodeCellsMap =  (Map<String, List<String>>) params.get("nodeCellsMap");
+
+        List<String> groupNameList = nodeMapper.getGroupNameList();
+
+        List<UnicomFormula> formulaList = formulaMapper.getFormulaLteList();
+
+        for (int j = 0; j < formulaList.size(); j ++) {
+
+            UnicomFormula formula = formulaList.get(j);
+
+            if (j != formulaList.size() -1) {
+
+                paramcloumns.add("formula" + formula.getId() + ",");
+            } else {
+
+                paramcloumns.add("formula" + formula.getId());
+            }
+        }
+
+        if (groupNameList.size() == 0) {
+
+            return result;
+        }
+
+        for (String groupName : groupNameList) {
+
+            StringBuffer paramValue = new StringBuffer();
+
+            List<String> groupAllNodeAllCellList = new ArrayList<>();
+
+            String level = null;
+
+            paramValue.append("'" + groupName + "'");
+            paramValue.append("'" + time + "'");
+            paramValue.append("'" + level + "'");
+
+            List<String> nodeNameList = nodeMapper.getNodeNameListByGroupName(groupName);
+
+            if (nodeNameList.size() == 0) {
+
+                continue;
+            }
+
+            for (String nodeName : nodeNameList) {
+
+                if (nodeCellsMap.containsKey(nodeName)) {
+
+                    List<String> nodeCells = nodeCellsMap.get(nodeName);
+
+                    for (String cell : nodeCells) {
+
+                        groupAllNodeAllCellList.add(cell);
+                    }
+                }
+            }
+
+            for (int j = 0; j < formulaList.size(); j ++) {
+
+                UnicomFormula formula = formulaList.get(j);
+
+                if (expressionSetMap.containsKey(formula.getQuota_name())) {
+
+                    List<String> variableList = expressionSetMap.get(formula.getQuota_name());
+
+                    String expression = formula.getExpression();
+
+                    boolean flag = false;
+
+                    for (int i = 0; i < variableList.size(); i ++) {
+
+                        String variable = variableList.get(i);
+
+                        String pmValue =
+                                counterHistoryMapper.
+                                        getSumCounterByCellsAndCounterAndTime(groupAllNodeAllCellList, variable, time);
+
+                        if (pmValue != null && i != variableList.size() - 1) {
+
+                            expression = expression.replaceAll(variable, pmValue);
+                        } else if (pmValue != null && i == variableList.size() - 1) {
+
+                            expression = expression.replaceAll(variable, pmValue);
+
+                            flag = true;
+                        } else {
+
+                            break;
+                        }
+                    }
+
+                    if (flag) {
+
+                        String value = null;
+
+                        try{
+
+                            Expression exp = new Expression(expression);
+
+                            Double doubleValue = Double.parseDouble(String.valueOf(exp.eval()));
+
+                            value = String.valueOf((double)Math.round(doubleValue*100)/100);
+                        }catch (Exception e){
+
+                            value = "-1";
+                            e.getStackTrace();
+                        }
+
+                        if (j != formulaList.size() -1) {
+
+                            paramValue.append("'" + value + "',");
+                        }else {
+
+                            paramValue.append("'" + value + "'");
+                        }
+
+                    } else {
+
+                        if (j != formulaList.size() -1) {
+
+                            paramValue.append("-1,");
+                        } else {
+
+                            paramValue.append("-1");
+                        }
+                    }
+                }
+            }
+
+            paramValues.add(paramValue.toString());
+        }
+
+        try {
+
+            quotaGroupMapper.addQuotaHistoryGroupList(paramcloumns, paramValues);
+
+            result = "SUCCESS";
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+            result = "FAIL";
+        }
+
+        return result;
+    }
+
     public JSONObject nodeCalculationOld(String time) {
 
         JSONObject resultJson = new JSONObject();
