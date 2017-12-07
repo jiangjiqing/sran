485bc8a85ec30372f221513fa36a96355ed61562
qing.hua <qing.hua@hongshenol.com>
2017-12-07 15:04:12 +0800

    add api

--- src/main/java/com/hongshen/sran_service/service/util/MapHelper.java
+++ src/main/java/com/hongshen/sran_service/service/util/MapHelper.java
@@ -1,4 +1,5 @@
 package com.hongshen.sran_service.service.util;
+
 import com.alibaba.fastjson.JSONObject;
 import java.text.DecimalFormat;
 import java.util.List;
@@ -7,23 +8,31 @@ import java.util.List;
  * Created by poplar on 12/7/17.
  */
 public class MapHelper {
-    public  JSONObject getGroupLocation(List<JSONObject> list,String keyLatitude,String keyLongitude){
+
+    public static JSONObject getGroupLocation(List<JSONObject> list,
+                                              String keyLatitude,
+                                              String keyLongitude){
         JSONObject result = new JSONObject();
         DecimalFormat df   = new DecimalFormat("######0.0000000000");
-        int num=0;
+        int count = 0;
         Double latitude = 0.0;
         Double longitude = 0.0;
-        for (int i=0;i<list.size();i++) {
-            if (list.get(i).getDouble(keyLatitude)!=0.0&& list.get(i).getDouble(keyLongitude)!=0.0
-                    &&list.get(i).getDouble(keyLatitude)!=null&& list.get(i).getDouble(keyLongitude)!=null) {
-                num++;
-                latitude  =latitude + list.get(i).getDouble(keyLatitude);
-                longitude =longitude + list.get(i).getDouble(keyLongitude);
+
+        for (JSONObject node : list) {
+            if (node.getDouble(keyLatitude)!=0.0 &&
+                    node.getDouble(keyLongitude)!=0.0 &&
+                    node.getDouble(keyLatitude)!=null &&
+                    node.getDouble(keyLongitude)!=null) {
+
+                count++;
+                latitude += node.getDouble(keyLatitude);
+                longitude += node.getDouble(keyLongitude);
             }
         }
-        result.put("latitude",df.format(latitude/num));
-        result.put("longitude",df.format(longitude/num));
-        //Double[] result= {Double.parseDouble(df.format(latitude/num)),Double.parseDouble(df.format(longitude/num))};
+
+        result.put("latitude", df.format(latitude/count));
+        result.put("longitude", df.format(longitude/count));
+
         return  result;
     }
 
